800a3ddaf9d400246242a7e595d0b22838a58ea8
RobertoPecurto <148451186+RobertoPecurto@users.noreply.github.com>
2025-02-06 09:08:24 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,68 @@
 $(document).ready(function() {
-    AOS.init({
-        duration: 1000,
-        offset: 100,
-        easing: 'ease-in-out',
+
+    // Header animation
+    $("header").css({
+        "position": "relative",
+        "top": "-100px",
     });
 
+    $("header").animate({
+        "top": "0",
+        "opacity": "1"
+    }, 500);
+
+    // Title animation
+    $('#web-text, #web-image, #developer-text').css({
+        "position": "relative",
+        "top": "250px",
+        "opacity": "0"
+    });
+
+    $('#web-text, #web-image, #developer-text').each(function(index) {
+        $(this).delay(index * 200).animate({
+            "top": "0",
+            "opacity": "1"
+        }, 300);
+    });
+
+    // Initializes smooth scroll with Locomotive Scroll (Plugin)
+    if (typeof LocomotiveScroll !== "undefined") {
+        const scrollContainer = document.querySelector("body");
+        
+        const locoScroll = new LocomotiveScroll({
+            el: scrollContainer,
+            smooth: true,
+            multiplier: 0.4,
+            touchMultiplier: 0.4,
+        });
+
+        // Horizontal movement for the links
+        locoScroll.on("scroll", (event) => {
+            const scrollPosition = event.scroll.y;
+            
+            const moveAmount = scrollPosition * 0.3;
+
+            $("#projects-link").css("transform", "translateX(" + (100 - moveAmount) + "%)");
+
+
+            $("#curriculum-link").css("transform", "translateX(" + (moveAmount - 100) + "%)");
+
+            const delayOffset = 50;
+            $("#about-me-link").css("transform", "translateX(" + (100 - (moveAmount - delayOffset)) + "%)");
+
+        });
+
+        console.log("Locomotive Scroll inicializado correctamente.");
+    } else {
+        console.error("Error: Locomotive Scroll no está definido.");
+    }
+
+    // Focus on the name when open
     $('#contactModal').on('shown.bs.modal', function () {
         $('#name').focus();
     });
 
+    // Validates and submits the contact form using AJAX
     $("#contactForm").on('submit', function(event) {
         event.preventDefault();
 
@@ -56,6 +110,35 @@ $(document).ready(function() {
             })
         }
 
+    });
+
+    // Border animation
+    $(".border-gray-right").each(function() {
+        let border = $("<div></div>").css({
+            position: "absolute",
+            top: "0",
+            left: "100%",
+            width: "100%",
+            height: "2px",
+            backgroundColor: "#777"
+        });
+
+        $(this).css("position", "relative").append(border);
+        border.animate({ left: "0" }, 2000);
+    });
+
+    $(".border-gray-left").each(function () {
+        let border = $("<div></div>").css({
+            position: "absolute",
+            top: "0",
+            right: "100%",
+            width: "100%",
+            height: "2px",
+            backgroundColor: "#777"
+        });
+
+        $(this).css("position", "relative").append(border);
+        border.animate({ right: "0" }, 2000);
     });
 
 });
